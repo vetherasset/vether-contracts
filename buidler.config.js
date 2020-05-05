8ec31da8650fa24dbe763bb96b38443f4089e41c
Strictly Scarce <strictly_scarce1@protonmail.com>
2020-05-05 12:57:14 +1000

    tweaks
    Signed-off-by: Strictly Scarce <strictly_scarce1@protonmail.com>

--- buidler.config.js
+++ buidler.config.js
@@ -1,7 +1,6 @@
 usePlugin("@nomiclabs/buidler-truffle5");
 
 module.exports = {
-    // This is a sample solc configuration that specifies which version of solc to use
     solc: {
       version: "0.6.4",
     },
