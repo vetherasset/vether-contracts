5bbdfe4465e37acfebf91f61c2b8de23d9902cd4
StrictlyScarce <strictly_scarce1@protonmail.com>
2020-07-01 15:54:54 +1000

    final vether
    Signed-off-by: StrictlyScarce <strictly_scarce1@protonmail.com>

--- test/9_upgrade4.js
+++ test/9_upgrade4.js
@@ -1,10 +1,10 @@
 const Vether1 = artifacts.require("./Vether1.sol")
 const Vether2 = artifacts.require("./Vether2.sol")
-const Vether3 = artifacts.require("./Vether4.sol")
+const Vether4 = artifacts.require("./Vether4.sol")
 const BigNumber = require('bignumber.js')
 const TruffleAssert = require('truffle-assertions')
 
-var vether1; var vether2; var vether3;
+var vether1; var vether2; var vether4;
 var acc0; var acc1; var acc2; var acc3; var accBurn;
 
 const upgradeHeight1 = 3
@@ -55,24 +55,24 @@ contract("Upgrade Vether", async accounts => {
 	transfer('v2', acc2) // send some to acc2
 	excludeVether('v2') //burnAddress in Vether2
 	snapshot('v2') //snapshot Vether2 in 3
-	upgradeTo3from1(acc1)
-	upgradeTo3from2(acc0)
-	upgradeTo3from2(acc2)
-	sendEther('v3', acc0) // 1-5 mine Vether3
-	sendEther('v3', acc0) // 1-6 mine Vether3
-	withdraws('v3', acc0, 1,5) // withdraw Vether3
-	withdraws('v3', acc0, 1,6) // withdraw Vether3
+	upgradeTo4from1(acc1)
+	upgradeTo4from2(acc0)
+	upgradeTo4from2(acc2)
+	sendEther('v4', acc0) // 1-5 mine Vether4
+	sendEther('v4', acc0) // 1-6 mine Vether4
+	withdraws('v4', acc0, 1,5) // withdraw Vether4
+	withdraws('v4', acc0, 1,6) // withdraw Vether4
 	transfer3(acc3) // send some to acc2
-	excludeVether('v3')
+	excludeVether('v4')
 	sendEther('v1', acc0) // 1-2
 	withdraws('v1', acc0, 1, 4)
 	sendEther('v2', acc0) // 1-4 mine Vether2
 	withdraws('v2', acc0, 1,6) // withdraw Vether2
-	failUpgradeTo3from1(acc0) // can't claim too much
-	failUpgradeTo3from2(acc0) // can't claim too much
+	failUpgradeTo4from1(acc0) // can't claim too much
+	failUpgradeTo4from2(acc0) // can't claim too much
 	sendEther('v2', acc2) // 1-4 mine Vether2
 	withdraws('v2', acc2, 1,7) // withdraw Vether2
-	failUpgradeTo3from2(acc2) // can't claim too much
+	failUpgradeTo4from2(acc2) // can't claim too much
 	purge()
 })
 
@@ -93,46 +93,46 @@ function constructor(accounts) {
 function deploy3(accounts) {
 	acc0 = accounts[0]; acc1 = accounts[1]; acc2 = accounts[2]; acc3 = accounts[3];
 	accBurn = acc2;
-	it("initializes Vether3 with correct params", async () => {
+	it("initializes Vether4 with correct params", async () => {
 
-		vether3 = await Vether3.new(vether1.address, vether2.address)
-		console.log('vether3', vether3.address)
+		vether4 = await Vether4.new(vether1.address, vether2.address)
+		console.log('vether4', vether4.address)
 
 		const genesisOld = BN2Str(await vether1.genesis())
-		const genesis = BN2Str(await vether3.genesis())
+		const genesis = BN2Str(await vether4.genesis())
 		assert.equal(genesis, genesisOld)
 		
-		const ndt3 = getBN(await vether3.nextDayTime())
+		const ndt3 = getBN(await vether4.nextDayTime())
 		console.log(BN2Str(ndt3))
 
 
-		const currentDay = await vether3.currentDay()
+		const currentDay = await vether4.currentDay()
 		assert.equal(currentDay, upgradeHeight2)
 
 		const totalFees = BN2Str(await vether2.totalFees())
-		assert.equal(totalFees, BN2Str(await vether3.totalFees()))
+		assert.equal(totalFees, BN2Str(await vether4.totalFees()))
 
 		const totalBurnt = BN2Str(await vether2.totalBurnt())
-		assert.equal(totalBurnt, BN2Str(await vether3.totalBurnt()))
+		assert.equal(totalBurnt, BN2Str(await vether4.totalBurnt()))
 
 		const totalSupply = BN2Str(await vether2.totalSupply())
-		assert.equal(totalSupply, BN2Str(await vether3.totalSupply()))
+		assert.equal(totalSupply, BN2Str(await vether4.totalSupply()))
 
-		const totalEmitted = BN2Str(await vether3.totalEmitted())
+		const totalEmitted = BN2Str(await vether4.totalEmitted())
 		assert.equal(totalEmitted, (upgradeHeight2 - 1) * 2048)
 
-		const remaining = BN2Str(await vether3.getRemainingAmount())
+		const remaining = BN2Str(await vether4.getRemainingAmount())
 		assert.equal(remaining, (upgradeHeight2 - 1) * 2048)
 		console.log('remaining', remaining)
 
 		for(let i =1; i<upgradeHeight1; i++){
 			const units = BN2Str(await vether1.mapEraDay_Units(1,i))
-			assert.equal(units, BN2Str(await vether3.mapEraDay_Units(1,i)))
+			assert.equal(units, BN2Str(await vether4.mapEraDay_Units(1,i)))
 			console.log(i, units)
 		}
 		for(let i =upgradeHeight1; i<upgradeHeight2; i++){
 			const units = BN2Str(await vether2.mapEraDay_Units(1,i))
-			assert.equal(units, BN2Str(await vether3.mapEraDay_Units(1,i)))
+			assert.equal(units, BN2Str(await vether4.mapEraDay_Units(1,i)))
 			console.log(i, units)
 		}
 
@@ -147,7 +147,7 @@ function sendEther(_vether, _acc) {
 		let vether
 		if (_vether == 'v1') { vether = vether1; }
 		else if (_vether == 'v2') { vether = vether2; }
-		else { vether = vether3; }
+		else { vether = vether4; }
 
 		var _era; var _day;
 
@@ -190,7 +190,7 @@ function withdraws(_vether, _acc, _era, _day) {
 		let vether
 		if (_vether == 'v1') { vether = vether1; }
 		else if (_vether == 'v2') { vether = vether2; }
-		else { vether = vether3; }
+		else { vether = vether4; }
 
 		let _bal = Emission
 
@@ -232,8 +232,8 @@ function transfer(_vether, _acc) {
 
 		let vether; let vetherNew;
 		if (_vether == 'v1') { vether = vether1; vetherNew=vether2}
-		else if (_vether == 'v2') { vether = vether2; vetherNew=vether3}
-		else { vether = vether3; vetherNew=vether3}
+		else if (_vether == 'v2') { vether = vether2; vetherNew=vether4}
+		else { vether = vether4; vetherNew=vether4}
 
 		let vetherOldBal1 = getBN(await vether.balanceOf(vether.address));
 		//console.log("vether Balance Start:", vetherOldBal1.toNumber());
@@ -278,13 +278,13 @@ function excludeVether(_vether) {
 			await vether.changeExcluded(burnAddress, { from: acc0 })
 			assert.equal(await vether.mapAddress_Excluded(burnAddress), true)
 		}
-		else { vether = vether3; 
+		else { vether = vether4; 
 			await vether.addExcluded(acc0, { from: acc0 })
 			assert.equal(await vether.mapAddress_Excluded(acc0), true)
-			console.log(BN2Str(await vether3.excludedCount()))
-			console.log((await vether3.excludedArray(0)))
-			console.log((await vether3.excludedArray(1)))
-			console.log((await vether3.excludedArray(2)))
+			console.log(BN2Str(await vether4.excludedCount()))
+			console.log((await vether4.excludedArray(0)))
+			console.log((await vether4.excludedArray(1)))
+			console.log((await vether4.excludedArray(2)))
 		}
 	})
 }
@@ -293,8 +293,8 @@ function snapshot(_vether) {
 	// it('fails from non-deployer', async () => {
 	// 	let vether; let vetherNew;
 	// 	if (_vether == 'v1') { vether = vether1; vetherNew=vether2}
-	// 	else if (_vether == 'v2') { vether = vether2; vetherNew=vether3}
-	// 	else { vether = vether3; }
+	// 	else if (_vether == 'v2') { vether = vether2; vetherNew=vether4}
+	// 	else { vether = vether4; }
 
 	// 	let owners = [acc0, acc1, acc2]
 	// 	let ownership = [Emission, Emission / 2, Emission / 4]
@@ -314,7 +314,7 @@ function snapshot(_vether) {
 			let ownership = [acc0Own, acc1Own, acc2Own]
 			await vetherNew.snapshot(owners, ownership)
 		}
-		else if (_vether == 'v2') { vether = vether2; vetherNew=vether3
+		else if (_vether == 'v2') { vether = vether2; vetherNew=vether4
 			let owners = [acc0, acc1, acc2]
 			let acc0Own1 = await vether1.balanceOf(acc0)
 			let acc1Own1 = await vether1.balanceOf(acc1)
@@ -327,7 +327,7 @@ function snapshot(_vether) {
 			let ownership2 = [acc0Own2, acc1Own2, acc2Own2]
 			await vetherNew.snapshot(owners, ownership2)
 		}
-		else { vether = vether3; }
+		else { vether = vether4; }
 		console.log('ownership acc0', BN2Str(await vetherNew.mapPreviousOwnership(acc0)))
 		console.log('ownership acc1', BN2Str(await vetherNew.mapPreviousOwnership(acc1)))
 		console.log('ownership acc2', BN2Str(await vetherNew.mapPreviousOwnership(acc2)))
@@ -340,7 +340,7 @@ function sendEtherFail(_vether) {
 		let vether
 		if (_vether == 'v1') {vether = vether1;} 
 		else if (_vether == 'v2') {vether = vether2;} 
-		else {vether = vether3;}
+		else {vether = vether4;}
 		// console.log(_vether)
 		// console.log(vether.address)
 		TruffleAssert.reverts(vether.send(sendEth, { from: acc0 }))
@@ -350,9 +350,9 @@ function sendEtherFail(_vether) {
 function upgradeTo2(_acc) {
 
 	// it("fails an upgrade for too much", async () => {
-	// 	console.log('ownership acc', BN2Str(await vether3.mapPreviousOwnership(_acc)))
-	// 	await vether.approve(vether3.address, '10000000', {from:_acc})
-	// 	TruffleAssert.reverts(vether3.upgrade('10000000'))
+	// 	console.log('ownership acc', BN2Str(await vether4.mapPreviousOwnership(_acc)))
+	// 	await vether.approve(vether4.address, '10000000', {from:_acc})
+	// 	TruffleAssert.reverts(vether4.upgrade('10000000'))
 	// })
 
 	it(`allows ${_acc} to upgrade all from Vether1 to Vether2`, async () => {
@@ -383,74 +383,74 @@ function upgradeTo2(_acc) {
 	})
 }
 
-function upgradeTo3from1(_acc) {
+function upgradeTo4from1(_acc) {
 
-	it("allows a Vether1 upgrade to Vether3 starts", async () => {
+	it("allows a Vether1 upgrade to Vether4 starts", async () => {
 		let balanceLeft = await vether1.balanceOf(_acc)
-		let remaining = getBN(await vether3.getRemainingAmount())
-		let upgradedAmount = getBN(await vether3.upgradedAmount())
+		let remaining = getBN(await vether4.getRemainingAmount())
+		let upgradedAmount = getBN(await vether4.upgradedAmount())
 		console.log('balanceLeft', BN2Str(balanceLeft))
-		console.log('ownership acc', BN2Str(await vether3.mapPreviousOwnership(_acc)))
-		await vether1.approve(vether3.address, balanceLeft, { from: _acc })
-		console.log('allowance', BN2Str(await vether1.allowance(_acc, vether3.address)))
+		console.log('ownership acc', BN2Str(await vether4.mapPreviousOwnership(_acc)))
+		await vether1.approve(vether4.address, balanceLeft, { from: _acc })
+		console.log('allowance', BN2Str(await vether1.allowance(_acc, vether4.address)))
 
 		let balanceBurn1 = getBN(await vether1.balanceOf(burnAddress))
-		await vether3.claim({ from: _acc })
+		await vether4.claim({ from: _acc })
 		let balanceBurn2 = getBN(await vether1.balanceOf(burnAddress))
 		
 		// assert.equal(BN2Str(balanceBurn2.minus(balanceBurn1)), BN2Str(balanceLeft))
-		let remainingEnd = getBN(await vether3.getRemainingAmount())
+		let remainingEnd = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingEnd)), balanceLeft.toString())
 		//console.log(BN2Str(remainingEnd))
 
-		let newUpgradedAmount = BN2Str(await vether3.upgradedAmount())
+		let newUpgradedAmount = BN2Str(await vether4.upgradedAmount())
 		assert.equal(newUpgradedAmount, BN2Str(upgradedAmount.plus(balanceLeft)))
 
-		const remainingNew = getBN(await vether3.getRemainingAmount())
+		const remainingNew = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingNew)), BN2Str(balanceLeft))
 		console.log('remainingNew', BN2Str(remainingNew))
-		console.log('balance', BN2Str(await vether3.balanceOf(_acc)))
-		console.log('holders', BN2Str(await vether3.holders()))
-		console.log('holder0', await vether3.holderArray(0))
+		console.log('balance', BN2Str(await vether4.balanceOf(_acc)))
+		console.log('holders', BN2Str(await vether4.holders()))
+		console.log('holder0', await vether4.holderArray(0))
 
 	})
 }
-function upgradeTo3from2(_acc) {
+function upgradeTo4from2(_acc) {
 
-	it("allows a Vether2 upgrade to Vether3", async () => {
+	it("allows a Vether2 upgrade to Vether4", async () => {
 		let balanceLeft = await vether2.balanceOf(_acc)
-		let remaining = getBN(await vether3.getRemainingAmount())
-		let upgradedAmount = getBN(await vether3.upgradedAmount())
+		let remaining = getBN(await vether4.getRemainingAmount())
+		let upgradedAmount = getBN(await vether4.upgradedAmount())
 		console.log('balanceLeft', BN2Str(balanceLeft))
-		console.log('ownership acc', BN2Str(await vether3.mapPreviousOwnership(_acc)))
-		await vether2.approve(vether3.address, balanceLeft, { from: _acc })
-		console.log('allowance', BN2Str(await vether2.allowance(_acc, vether3.address)))
+		console.log('ownership acc', BN2Str(await vether4.mapPreviousOwnership(_acc)))
+		await vether2.approve(vether4.address, balanceLeft, { from: _acc })
+		console.log('allowance', BN2Str(await vether2.allowance(_acc, vether4.address)))
 
 		let balanceBurn1 = getBN(await vether2.balanceOf(burnAddress))
-		await vether3.claim({ from: _acc })
+		await vether4.claim({ from: _acc })
 		let balanceBurn2 = getBN(await vether2.balanceOf(burnAddress))
 		
 		// assert.equal(BN2Str(balanceBurn2.minus(balanceBurn1)), BN2Str(balanceLeft))
-		let remainingEnd = getBN(await vether3.getRemainingAmount())
+		let remainingEnd = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingEnd)), balanceLeft.toString())
 		//console.log(BN2Str(remainingEnd))
 
-		let newUpgradedAmount = BN2Str(await vether3.upgradedAmount())
+		let newUpgradedAmount = BN2Str(await vether4.upgradedAmount())
 		assert.equal(newUpgradedAmount, BN2Str(upgradedAmount.plus(balanceLeft)))
 
-		const remainingNew = getBN(await vether3.getRemainingAmount())
+		const remainingNew = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingNew)), BN2Str(balanceLeft))
 		console.log('remainingNew', BN2Str(remainingNew))
-		console.log('balance', BN2Str(await vether3.balanceOf(_acc)))
-		console.log('holders', BN2Str(await vether3.holders()))
-		console.log('holder0', await vether3.holderArray(0))
+		console.log('balance', BN2Str(await vether4.balanceOf(_acc)))
+		console.log('holders', BN2Str(await vether4.holders()))
+		console.log('holder0', await vether4.holderArray(0))
 
 	})
 }
 
 function transfer3(_acc) {
 	it("allows a transfer between accounts for fees", async () => {
-		let vether = vether3
+		let vether = vether4
 		let vetherOldBal1 = getBN(await vether.balanceOf(vether.address));
 		//console.log("vether Balance Start:", vetherOldBal1.toNumber());
 		let acc0Bal1 = getBN(await vether.balanceOf(acc0));
@@ -478,71 +478,71 @@ function transfer3(_acc) {
 	})
 }
 
-function failUpgradeTo3from1(_acc) {
+function failUpgradeTo4from1(_acc) {
 
-	it("fails a Vether1 upgrade to Vether3", async () => {
+	it("fails a Vether1 upgrade to Vether4", async () => {
 		let balanceLeft = await vether1.balanceOf(_acc)
-		let remaining = getBN(await vether3.getRemainingAmount())
-		let upgradedAmount = getBN(await vether3.upgradedAmount())
+		let remaining = getBN(await vether4.getRemainingAmount())
+		let upgradedAmount = getBN(await vether4.upgradedAmount())
 		console.log('balanceLeft', BN2Str(balanceLeft))
-		console.log('ownership acc', BN2Str(await vether3.mapPreviousOwnership(_acc)))
-		await vether1.approve(vether3.address, balanceLeft, { from: _acc })
-		console.log('allowance', BN2Str(await vether1.allowance(_acc, vether3.address)))
+		console.log('ownership acc', BN2Str(await vether4.mapPreviousOwnership(_acc)))
+		await vether1.approve(vether4.address, balanceLeft, { from: _acc })
+		console.log('allowance', BN2Str(await vether1.allowance(_acc, vether4.address)))
 
 		let balanceBurn1 = getBN(await vether1.balanceOf(burnAddress))
-		await vether3.claim({ from: _acc })
+		await vether4.claim({ from: _acc })
 		let balanceBurn2 = getBN(await vether1.balanceOf(burnAddress))
 		
 		// assert.equal(BN2Str(balanceBurn2.minus(balanceBurn1)), BN2Str(0))
-		let remainingEnd = getBN(await vether3.getRemainingAmount())
+		let remainingEnd = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingEnd)), BN2Str(0))
 		//console.log(BN2Str(remainingEnd))
 
-		let newUpgradedAmount = BN2Str(await vether3.upgradedAmount())
+		let newUpgradedAmount = BN2Str(await vether4.upgradedAmount())
 		assert.equal(newUpgradedAmount, BN2Str(upgradedAmount.plus(0)))
 
-		const remainingNew = getBN(await vether3.getRemainingAmount())
+		const remainingNew = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingNew)), BN2Str(0))
 		console.log('remainingNew', BN2Str(remainingNew))
-		console.log('balance', BN2Str(await vether3.balanceOf(_acc)))
-		console.log('holders', BN2Str(await vether3.holders()))
-		console.log('holder0', await vether3.holderArray(0))
-		console.log('holder1', await vether3.holderArray(1))
-		console.log('holder2', await vether3.holderArray(2))
-		console.log('holder3', await vether3.holderArray(3))
-		console.log('holder4', await vether3.holderArray(4))
+		console.log('balance', BN2Str(await vether4.balanceOf(_acc)))
+		console.log('holders', BN2Str(await vether4.holders()))
+		console.log('holder0', await vether4.holderArray(0))
+		console.log('holder1', await vether4.holderArray(1))
+		console.log('holder2', await vether4.holderArray(2))
+		console.log('holder3', await vether4.holderArray(3))
+		console.log('holder4', await vether4.holderArray(4))
 
 	})
 }
-function failUpgradeTo3from2(_acc) {
+function failUpgradeTo4from2(_acc) {
 
-	it("fails a Vether2 upgrade to Vether3", async () => {
+	it("fails a Vether2 upgrade to Vether4", async () => {
 		let balanceLeft = await vether2.balanceOf(_acc)
-		let remaining = getBN(await vether3.getRemainingAmount())
-		let upgradedAmount = getBN(await vether3.upgradedAmount())
+		let remaining = getBN(await vether4.getRemainingAmount())
+		let upgradedAmount = getBN(await vether4.upgradedAmount())
 		console.log('balanceLeft', BN2Str(balanceLeft))
-		console.log('ownership acc', BN2Str(await vether3.mapPreviousOwnership(_acc)))
-		await vether2.approve(vether3.address, balanceLeft, { from: _acc })
-		console.log('allowance', BN2Str(await vether2.allowance(_acc, vether3.address)))
+		console.log('ownership acc', BN2Str(await vether4.mapPreviousOwnership(_acc)))
+		await vether2.approve(vether4.address, balanceLeft, { from: _acc })
+		console.log('allowance', BN2Str(await vether2.allowance(_acc, vether4.address)))
 
 		let balanceBurn1 = getBN(await vether2.balanceOf(burnAddress))
-		await vether3.claim({ from: _acc })
+		await vether4.claim({ from: _acc })
 		let balanceBurn2 = getBN(await vether2.balanceOf(burnAddress))
 		
 		// assert.equal(BN2Str(balanceBurn2.minus(balanceBurn1)), BN2Str(0))
-		let remainingEnd = getBN(await vether3.getRemainingAmount())
+		let remainingEnd = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingEnd)), BN2Str(0))
 		//console.log(BN2Str(remainingEnd))
 
-		let newUpgradedAmount = BN2Str(await vether3.upgradedAmount())
+		let newUpgradedAmount = BN2Str(await vether4.upgradedAmount())
 		assert.equal(newUpgradedAmount, BN2Str(upgradedAmount.plus(0)))
 
-		const remainingNew = getBN(await vether3.getRemainingAmount())
+		const remainingNew = getBN(await vether4.getRemainingAmount())
 		assert.equal(BN2Str(remaining.minus(remainingNew)), BN2Str(0))
 		console.log('remainingNew', BN2Str(remainingNew))
-		console.log('balance', BN2Str(await vether3.balanceOf(_acc)))
-		console.log('holders', BN2Str(await vether3.holders()))
-		console.log('holder0', await vether3.holderArray(0))
+		console.log('balance', BN2Str(await vether4.balanceOf(_acc)))
+		console.log('holders', BN2Str(await vether4.holders()))
+		console.log('holder0', await vether4.holderArray(0))
 
 	})
 }
@@ -550,12 +550,12 @@ function failUpgradeTo3from2(_acc) {
 function purge() {
 	it('purge', async () => {
 		let owners = [acc0, acc1, acc2]
-		let acc0Own = await vether3.balanceOf(acc0)
-		let acc1Own = await vether3.balanceOf(acc1)
-		let acc2Own = await vether3.balanceOf(acc2)
+		let acc0Own = await vether4.balanceOf(acc0)
+		let acc1Own = await vether4.balanceOf(acc1)
+		let acc2Own = await vether4.balanceOf(acc2)
 		let ownership = [acc0Own, acc1Own, acc2Own]
-		await vether3.purgeDeployer()
-		TruffleAssert.reverts(vether3.snapshot(owners, ownership, { from: acc0 }))
+		await vether4.purgeDeployer()
+		TruffleAssert.reverts(vether4.snapshot(owners, ownership, { from: acc0 }))
 
 	})
 }
